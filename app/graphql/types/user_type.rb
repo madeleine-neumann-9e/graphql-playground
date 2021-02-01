6ac84f299e5a9e2fcfbdb5f005a3a1d60b27912c
Madeleine Neumann <madeleine.neumann@9elements.com>
2021-02-01 12:43:24 +0100

    create user type

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -6,5 +6,16 @@ module Types
     field :email, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    # Array of posts the User created
+    field :posts, [Types::PostType], null: true
+
+    # Own Method
+    field :posts_count, Integer, null: true
+
+    def posts_count
+      object.posts.count
+    end
+
   end
 end
